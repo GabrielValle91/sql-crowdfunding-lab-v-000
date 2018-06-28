52c34e6569d73a4553a654d55e8dbb03ed9824bd
Gabriel Valle <gvalle91@gmail.com>
2018-06-28 00:29:42 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -23,7 +23,7 @@ ORDER BY projects.title"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
-"SELECT users.name, pledges.amount FROM users 
+"SELECT users.name, pledges.amount FROM users
 INNER JOIN pledges ON users.id = pledges.user_id
 GROUP BY users.name
 ORDER BY pledges.amount, users.name;"
